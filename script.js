44c93c26bec2ac03c520dbed48970d0479fcf201
Eblckwood <142993037+Eblckwood@users.noreply.github.com>
2024-04-19 08:33:46 -0400

    yes

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ function showStats() {
 
     switch (selectedPlayer) {
         case "patty":
-            stats = "Patrick M: Passing Yards 4183 - Passing Td's 27 - Super Bowl Winner";
+            stats = "Patrick Mahomes: Passing Yards 4183 - Passing Touchdown's 27 - Super Bowl Winner";
             break;
         case "Joyy":
-            stats = "Joe Burrow: Passing Yards 2309 - Passing Td's 15";
+            stats = "Joe Burrow: Passing Yards 2309 - Passing Touchdown's 15";
             break;
         case "CJ":
-            stats = "CJ: Passing Yards 4108 - Passing Td's 23 - Rookie of the Year";
+            stats = "C.J Stroud: Passing Yards 4108 - Passing Touchdown's 23 - Rookie of the Year";
             break;
         case "Christen":
-            stats = "CMC: Rushing Yards 1459 - Total Td's 21 - Offensive Player of the Year";
+            stats = "Christen McCaffrey: Rushing Yards 1459 - Total Touchdown's 21 - Offensive Player of the Year";
             break;
-        case "Travis":
-            stats = "Travis Kelce: Receiving Yards 984 - Total Td's 5";
+        case "Travis Kelce":
+            stats = "Travis Kelce: Receiving Yards 984 - Total Touchdown's 5";
             break;
         case "KING":
-            stats = "Derick Henry: Rushing Yards 1167 - Rushing Td's 12";
+            stats = "Derick Henry: Rushing Yards 1167 - Rushing Touchdown's 12";
             break;
         default:
             stats = "";
